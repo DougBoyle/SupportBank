8b78973473f993e69cb4b1310d3b3c3ed7d9b199
DougBoyle <dab80@cam.ac.uk>
2020-07-08 09:21:03 +0100

    Date object rather than string

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -11,85 +11,13 @@ using NLog.Targets;
 namespace SupportBank {
     internal class Program {
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
-
         private static List<Transaction> transactions = new List<Transaction>();
-        
-        public static void ListAll() {
-            var accounts = new Dictionary<string, decimal>(); // make an object, use decimal as appose to float?
-            foreach (var t in transactions) {
-                if (!accounts.ContainsKey(t.From)) {
-                    accounts.Add(t.From, decimal.Zero);
-                }
-                if (!accounts.ContainsKey(t.To)) {
-                    accounts.Add(t.To, decimal.Zero);
-                }
-                accounts[t.From] -= t.Amount;
-                accounts[t.To] += t.Amount;
-            }
-            foreach (var entry in accounts) {
-                Console.WriteLine($"{entry.Key}: {entry.Value}"); // string interpolation
-            }
-        }
-
-        public static void ListAccount(string name) {
-            transactions.FindAll(t => t.From == name || t.To == name)  
-               .ForEach(t => Console.WriteLine(t));
-        }
-        
-        public static void readCsv(string filename) {
-            transactions = new List<Transaction>();
-            
-            using(var reader = new StreamReader(filename)) {
-                reader.ReadLine(); // header
-                var lineNum = 1;
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    lineNum++;
-                    var strings = line.Split(',');
-                    if (strings.Length != 5) {
-                        Console.WriteLine($"Malformed transaction - line {lineNum}: {line}");
-                        continue;
-                    }
-                    try {
-                        var date = DateTime.Parse(strings[0]);
-                        transactions.Add(new Transaction(strings[0], strings[1], 
-                            strings[2], strings[3], decimal.Parse(strings[4])));
-                    } catch {
-                        Console.WriteLine($"Malformed transaction - line {lineNum}: {line}");
-                    }
-                }
-            }
-        }
-
-        public static void readJson(string filename) {
-            transactions = JsonConvert.DeserializeObject<List<Transaction>>(
-                File.ReadAllText(filename));
-        }
-
-        public static void readXML(string filename)
-        {
-            transactions = new List<Transaction>();
-            
-            var xmldoc = new XmlDocument();
-            xmldoc.LoadXml(File.ReadAllText(filename));
-            XmlNodeList nodes = xmldoc.DocumentElement.SelectNodes("/TransactionList/SupportTransaction");
-            foreach (XmlNode node in nodes)
-            {
-                var Narrative = node.SelectSingleNode("Description").InnerText;
-                var Amount = decimal.Parse(node.SelectSingleNode("Value").InnerText);
-                var From = node.SelectSingleNode("Parties/From").InnerText;
-                var To = node.SelectSingleNode("Parties/To").InnerText;
-                var Date = node.Attributes[0].Value;
-                var t = new Transaction(Date, From, To, Narrative, Amount);
-                transactions.Add(t);
-            }
-        }
         
         public static void Main()
         {
             var config = new LoggingConfiguration();
-            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log",
+                Layout = @"${longdate} ${level} - ${logger}: ${message}" };
             config.AddTarget("File Logger", target);
             config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
             LogManager.Configuration = config;
@@ -131,6 +59,78 @@ namespace SupportBank {
                 { 
                     Console.WriteLine("Incorrect Format");
                 }
+            }
+        }
+        
+        public static void ListAll() {
+            var accounts = new Dictionary<string, decimal>(); // make an object, use decimal as appose to float?
+            foreach (var t in transactions) {
+                if (!accounts.ContainsKey(t.From)) {
+                    accounts.Add(t.From, decimal.Zero);
+                }
+                if (!accounts.ContainsKey(t.To)) {
+                    accounts.Add(t.To, decimal.Zero);
+                }
+                accounts[t.From] -= t.Amount;
+                accounts[t.To] += t.Amount;
+            }
+            foreach (var entry in accounts) {
+                Console.WriteLine($"{entry.Key}: {entry.Value}"); // string interpolation
+            }
+        }
+
+        public static void ListAccount(string name) {
+            transactions.FindAll(t => t.From == name || t.To == name)  
+               .ForEach(t => Console.WriteLine(t));
+        }
+        
+        public static void readCsv(string filename) {
+            transactions = new List<Transaction>();
+            
+            using(var reader = new StreamReader(filename)) {
+                reader.ReadLine(); // header
+                var lineNum = 1;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    lineNum++;
+                    var strings = line.Split(',');
+                    if (strings.Length != 5) {
+                        Console.WriteLine($"Malformed transaction - line {lineNum}: {line}");
+                        continue;
+                    }
+                    try {
+                        var date = DateTime.Parse(strings[0]);
+                        transactions.Add(new Transaction(DateTime.Parse(strings[0]), strings[1], 
+                            strings[2], strings[3], decimal.Parse(strings[4])));
+                    } catch {
+                        Console.WriteLine($"Malformed transaction - line {lineNum}: {line}");
+                    }
+                }
+            }
+        }
+
+        public static void readJson(string filename) {
+            transactions = JsonConvert.DeserializeObject<List<Transaction>>(
+                File.ReadAllText(filename));
+        }
+
+        public static void readXML(string filename)
+        {
+            var baseDate = new DateTime(1900, 1, 1);
+            transactions = new List<Transaction>();
+            var xmldoc = new XmlDocument();
+            xmldoc.LoadXml(File.ReadAllText(filename));
+            XmlNodeList nodes = xmldoc.DocumentElement.SelectNodes("/TransactionList/SupportTransaction");
+            foreach (XmlNode node in nodes)
+            {
+                var Narrative = node.SelectSingleNode("Description").InnerText;
+                var Amount = decimal.Parse(node.SelectSingleNode("Value").InnerText);
+                var From = node.SelectSingleNode("Parties/From").InnerText;
+                var To = node.SelectSingleNode("Parties/To").InnerText;
+                var Date =  baseDate.AddDays(Int32.Parse(node.Attributes[0].Value));
+                var t = new Transaction(Date, From, To, Narrative, Amount);
+                transactions.Add(t);
             }
         }
     }
--- SupportBank/Transaction.cs
+++ SupportBank/Transaction.cs
@@ -1,12 +1,14 @@
-﻿namespace SupportBank {
+﻿using System;
+
+namespace SupportBank {
     public class Transaction {
-        internal string Date { get; } 
+        internal DateTime Date { get; } 
         internal string From { get; }  
         internal string To { get; }  
         internal string Narrative { get; } 
         internal decimal Amount { get; } 
 
-        public Transaction (string Date, string FromAccount, string ToAccount, string Narrative, decimal Amount)
+        public Transaction (DateTime Date, string FromAccount, string ToAccount, string Narrative, decimal Amount)
         {
             this.Date = Date; 
             this.From = FromAccount ; 
@@ -16,7 +18,7 @@
         }
 
         public override string ToString() {
-            return $"From: {From}, To: {To}, Amt: {Amount}, Date: {Date}, For: {Narrative}";
+            return $"From: {From}, To: {To}, Amt: {Amount}, Date: {Date.ToString("dd/MM/yyyy")}, For: {Narrative}";
         }
     }
 }
